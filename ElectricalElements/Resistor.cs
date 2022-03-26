f40be5f68c94fa5735a4d9c50b1d3e8fd6c57cb7
Naumkin <naumkin_1998@mail.ru>
2022-03-26 15:48:22 +0700

    Start lab

---
+++ ElectricalElements/Resistor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace ElectricalElements
+{
+    /// <summary>
+    /// Класс резистор
+    /// </summary>
+    public class Resistor
+    {
+        /// <summary>
+        /// Номинальное значение сопротивления
+        /// </summary>
+        private int _nominalValue;
+
+        /// <summary>
+        /// Рабочее напряжение
+        /// </summary>
+        private int _operatingVoltage;
+        
+        
+        /// <summary>
+        /// Рассеиваемая мощность
+        /// </summary>
+        private int _powerDissipation;
+
+        /// <summary>
+        /// Тип резистора
+        /// </summary>
+        public TypeOfResistor TypeOfResistor { get; set; }
+
+        /// <summary>
+        /// Номинальное значение сопротивления
+        /// </summary>
+        public int NominalValue
+        {
+            get => _nominalValue;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Сопротивление резистора " +
+                                                "не может быть отрицательным!");
+                }
+
+                _nominalValue = value;
+            }
+        }
+
+        public int OperatingVoltage
+        {
+            get => _operatingVoltage;
+            set
+            {
+
+                if (value < 0)
+                {
+                    throw new ArgumentException("Рабочее напряжение резистора " +
+                                                "не может быть отрицательным!");
+                }
+
+                _operatingVoltage = value;
+            }
+        }
+
+        public int PowerDissipation
+        {
+            get => _powerDissipation;
+            set
+            {
+
+                if (value < 0)
+                {
+                    throw new ArgumentException("Мощность рассеивания резистора " +
+                                                "не может быть отрицательным!");
+                }
+
+                _powerDissipation = value;
+            }
+        }
+
+        /// <summary>
+        /// Конструктор резистора
+        /// </summary>
+        /// <param name="nominalValue">Номинальная величина сопротивления</param>
+        /// <param name="operatingVoltage">Рабочее напряжение</param>
+        /// <param name="powerDissipation">Мощность рассивания</param>
+        /// <param name="typeOfResistor">Тип резистора</param>
+        public Resistor(int nominalValue, int operatingVoltage, int powerDissipation, TypeOfResistor typeOfResistor)
+        {
+            NominalValue = nominalValue;
+            OperatingVoltage = operatingVoltage;
+            PowerDissipation = powerDissipation;
+            TypeOfResistor = typeOfResistor;
+        }
+
+    }
+}
---
+++ ElectricalElements/TypeOfResistor.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace ElectricalElements
+{
+    /// <summary>
+    /// Типы резисторов
+    /// </summary>
+    public enum TypeOfResistor
+    {
+        [Description("Линейный")]
+        Linear,
+        [Description("Варистор")]
+        Varistor,
+        [Description("Терморезистор")]
+        Thermistors,
+        [Description("Фоторезистор")]
+        Photoresistors,
+        [Description("Тензорезистор")]
+        Tensoresistors,
+        [Description("Магниторезистор")]
+        Magnetoresistors
+    }
+}
