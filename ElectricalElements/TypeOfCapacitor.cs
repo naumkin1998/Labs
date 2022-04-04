b4342c16cbbe517d9f4c5a2bbd3a55abc817619e
Naumkin <naumkin_1998@mail.ru>
2022-04-04 15:09:50 +0700

    12

---
+++ ElectricalElements/TypeOfCapacitor.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace ElectricalElements
+{
+    public  enum TypeOfCapacity
+    {
+        [Description("Постоянная емкость")]
+        CConstantCapacity,
+        [Description("Переменная емкость")]
+        VariableCapacity
+    }
+}
